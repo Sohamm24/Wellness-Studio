4d31cc2b11a8df649ebe559429bbcaa97e417ea2
Soham Narvankar <168579451+Sohamm24@users.noreply.github.com>
2024-05-22 15:38:00 +0530

    New with updated buttons

---
+++ DietPlans.js
@@ -0,0 +1,16 @@
+let databaseB = [
+    { id: 1, name: 'Bread-Butter', calorie:250 },
+    { id: 2, name: 'Pohe', calorie: 250 },
+    { id: 3, name: 'Uppam', calorie: 250 },
+    { id: 4, name: 'Coffee', calorie: 100},
+    { id: 1, name: 'Tea', calorie: 100},
+];
+let databaseL = [
+    { id: 1, name: 'Bread-Butter', calorie: 30 },
+];
+let databaseD = [
+    { id: 1, name: 'Bread-Butter', calorie: 30 },
+];
+let databaseS = [
+    { id: 1, name: 'Bread-Butter', calorie: 30 },
+];
